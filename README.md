7b6b3b9063c5b228bef5aa5fe4079d425166cc44
wilump <wilump@dunamu.com>
2024-09-01 23:44:26 +0900

    [inventory] Redis를 통한 동시성 제어 (Lettuce, Redission) (#3)
    * [inventory] Redis를 통한 동시성 제어
    
    * readme 업데이트

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Java 동시성
+# Java 멀티 스레드와 동시성
+
+- [자바 애플리케이션의 동시성 제어](./inventory-service/README.md)
+  - `synchronized` 키워드
+  - MySQL을 활용한 동시성 제어
+      - Pessimistic Lock(비관적 락)
+      - Optimistic Lock(낙관적 락)
+      - Named Lock(네임드 락)
+  - Redis를 활용한 동시성 제어
+      - Lettuce를 이용한 분산 락
+      - Redission을 이용한 분산 락
---
+++ inventory-service/src/main/java/labs/wilump/inventory/facade/LettuceLockStockFacade.java
@@ -0,0 +1,30 @@
+package labs.wilump.inventory.facade;
+
+import labs.wilump.inventory.repository.RedisLockRepository;
+import labs.wilump.inventory.service.StockService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LettuceLockStockFacade {
+
+    private final RedisLockRepository redisLockRepository;
+
+    private final StockService stockService;
+
+    public LettuceLockStockFacade(RedisLockRepository redisLockRepository, StockService stockService) {
+        this.redisLockRepository = redisLockRepository;
+        this.stockService = stockService;
+    }
+
+    public void decrease(Long key, Long quantity) throws InterruptedException {
+        while (!redisLockRepository.lock(key)) {
+            Thread.sleep(100);
+        }
+
+        try {
+            stockService.decrease(key, quantity);
+        } finally {
+            redisLockRepository.unlock(key);
+        }
+    }
+}
