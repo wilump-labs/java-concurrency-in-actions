ab12a5323d22b5b52c278a43decba7beebf6930f
Heedong <59307414+ruthetum@users.noreply.github.com>
2024-09-01 20:34:33 +0900

    [inventory] 불필요 코드 제거

--- inventory-service/src/main/java/labs/wilump/inventory/service/SynchronizedStockService.java
+++ inventory-service/src/main/java/labs/wilump/inventory/service/SynchronizedStockService.java
@@ -21,23 +21,3 @@ public class SynchronizedStockService {
     }
 
 }
-
-
-public class TxSynchronizedStockService {
-
-        private final SynchronizedStockService stockService;
-
-        public TxSynchronizedStockService(SynchronizedStockService stockService) {
-            this.stockService = stockService;
-        }
-
-        public void decrease(Long id, Long quantity) {
-            this.startTransaction();
-
-            // 아래 함수에 대해서는 동시 접근이 제한되지만,
-            // 함수 호출 후 커밋이 되어 DB에 반영되기 전에 다른 스레드에서 접근할 수 있음
-            stockService.decrease(id, quantity);
-
-            this.endTransaction();
-        }
-}
